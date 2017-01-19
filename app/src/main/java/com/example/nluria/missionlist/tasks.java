33f68629e059406e06fc534f31fa729532f488b8
nluria <2nirluria@gmail.com>
2017-01-19 16:56:32 +0200

    added ability to add a new task and print it to screen.

--- app/src/main/java/com/example/nluria/missionlist/tasks.java
+++ app/src/main/java/com/example/nluria/missionlist/tasks.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,38 +15,59 @@ public class tasks extends AppCompatActivity
 {
     String nameOfGroup;
     Button btnAddTask;
+    EditText input;
 
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
+        //print title of group on screen.
         Intent intent = getIntent();
         nameOfGroup= intent.getStringExtra("name");
         TextView title= (TextView) findViewById(R.id.title);
         title.setText(nameOfGroup);
 
-        btnAddTask = (Button)findViewById(R.id.Add_btn);
-        addNewTask();
-
+        addTask();
     }
 
-    public void addNewTask()
+    public void addTask()
     {
-        btnAddTask.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View v)
-                    {
-                        System.out.println(nameOfGroup);
+        //create alert dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add a new task:");
+        builder.setMessage("");
+        input= new EditText(this);
+        builder.setView(input);
+
+        //set negative button.
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        //set positive button.
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String txt = input.getText().toString();
+                Toast.makeText(getApplicationContext(),txt, Toast.LENGTH_LONG).show();
+                input.setText("");
+            }
+        });
 
 
-                    }
-                }
-        );
+        final AlertDialog alertDialog= builder.create();
+
+
+        btnAddTask= (Button)findViewById(R.id.Add_btn);
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.show();
+            }
+        });
     }
-
 
 }
